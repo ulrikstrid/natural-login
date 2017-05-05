a10d5c417150d5c1e593f999fff695eb52450cc1
Ulrik Augustsson <ulrikaugustsson@gmail.com>
2017-05-05 13:50:42 +0200

    Add Email model with tests

---
+++ src/models/Email.test.ts
@@ -0,0 +1,45 @@
+import * as Email from './Email'
+
+test('emailRegex matches valid emails', () => {
+  const matchedEmails = Email.standardEmailDomains
+    .map((domain) => `ulrik.strid@${domain}`)
+    .map((validEmail) => Email.emailRegex.test(validEmail))
+
+  expect(matchedEmails).toEqual(expect.arrayContaining([true, true, true, true, true]))
+})
+
+test('emailRegex gives false for invalid emails', () => {
+  const unmatchedEmail = Email.standardEmailDomains
+    .map((domain) => `ulrik.strid${domain}`)
+    .map((nonValidEmail) => Email.emailRegex.test(nonValidEmail))
+
+  expect(unmatchedEmail).toEqual(expect.arrayContaining([false, false, false, false, false]))
+})
+
+test('validate email', () => {
+  expect(Email.validateEmail('ulrik.strid@outlook.com')).toEqual({ input: 'ulrik.strid@outlook.com', valid: true })
+  expect(Email.validateEmail('ulrik.stridoutlook.com')).toEqual({ input: 'ulrik.stridoutlook.com', valid: false })
+})
+
+test('string diff', () => {
+  const gmail = 'gmail.com'
+  const gnail = 'gnail.com'
+
+  const longerStringA = 'something longer with more diffs'
+  const longerStringB = 'somehting lnger  with more difs'
+
+  expect(Email.stringDiff(gmail, gnail)).toBe(1)
+  expect(Email.stringDiff(longerStringA, longerStringB)).toBe(2 + 5 + 0 + 0 + 2) // diffs per word summed
+})
+
+test('best match', () => {
+  const outlook = 'outlook.com'
+  const outjook = 'outjook.com'
+  const gmail = 'gmail.com'
+  const gnail = 'gnail.com'
+
+  expect(Email.bestMatch(Email.standardEmailDomains, outlook)).toEqual({ word: outlook, bestMatch: 'outlook.com', diff: 0})
+  expect(Email.bestMatch(Email.standardEmailDomains, outjook)).toEqual({ word: outjook, bestMatch: 'outlook.com', diff: 1})
+  expect(Email.bestMatch(Email.standardEmailDomains, gmail)).toEqual({ word: gmail, bestMatch: 'gmail.com', diff: 0})
+  expect(Email.bestMatch(Email.standardEmailDomains, gnail)).toEqual({ word: gnail, bestMatch: 'gmail.com', diff: 1})
+})
---
+++ src/models/Email.ts
@@ -0,0 +1,50 @@
+export type Email = string
+
+export type HelpMessage = {
+  email: Email,
+  valid: boolean
+}
+
+export const emailRegex = /[a-z\.]+@[a-z]+\.(com|se)/
+
+export function validateEmail (email: string): { input: Email, valid: boolean } {
+  return {
+    input: email,
+    valid: emailRegex.test(email)
+  }
+}
+
+export const standardEmailDomains = [
+  'gmail.com',
+  'outlook.com',
+  'hotmail.com',
+  'live.com',
+  'telia.se'
+]
+
+export function stringDiff (stringA: string, stringB: string): number {
+  return stringA.split('')
+    .map((char, index) => {
+      return stringB[index] === char ? 0 : 1
+    })
+    .reduce((acc, curr) => acc + curr, 0)
+}
+
+export function bestMatch (list: string[], word: string): { word: string, bestMatch: string, diff: number } {
+  const bestDiff = list
+    .map((item) => {
+      return {
+        item,
+        diff: stringDiff(word, item)
+      }
+    })
+    .reduce((acc, curr) => {
+      if (acc.diff < curr.diff) {
+        return acc
+      }
+
+      return curr
+    })
+
+  return { word, bestMatch: bestDiff.item, diff: bestDiff.diff }
+}
